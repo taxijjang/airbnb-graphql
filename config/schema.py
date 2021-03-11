4322c985673031c1800050bc737a28988ea167ac
taxijjang <gw9122@naver.com>
2021-03-11 18:37:23 +0900

    Queries Using Graphene Django

--- config/schema.py
+++ config/schema.py
@@ -1,7 +1,25 @@
 import graphene
+from graphene_django import DjangoObjectType
+from rooms.models import Room
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
 
 class Query(graphene.ObjectType):
-    pass
+    hello = graphene.String()
+    taxijjang = graphene.String()
+    rooms = graphene.List(RoomType)
+
+    def resolve_hello(self, info):
+        print(info)
+        return "Hello"
+
+    def resolve_taxijjang(self, info):
+        return "택시짱"
+
+    def resolve_rooms(self, info):
+        return Room.objects.all()
 
 class Mutaion:
     pass
